c125a39fa801b8616704599a385c1d28782f351e
jfpaezl <108756151+jfpaezl@users.noreply.github.com>
2023-04-15 20:25:12 -0500

    finalizado

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,28 +1,49 @@
 """ buscar la columna de un encabezado"""
 def buscar_columna(fila : int, encabezado : str, hoja):
-    # Identificar en qué fila se encuentra el encabezado
-    fila_encabezado = fila  # Por ejemplo, si el encabezado está en la primera fila
-
     # Recorrer las celdas de la fila buscando el encabezado con el nombre específico
-    encabezado_buscado = encabezado  # Por ejemplo, buscamos el encabezado 'Nombre'
+    encabezado_buscado = encabezado  # Por ejemplo, buscamos el encabezado 'Cliente'
     columna_encabezado = None
     for columna in range(1, hoja.max_column + 1):
-        celda = hoja.cell(row=fila_encabezado, column=columna)
+        celda = hoja.cell(row=fila, column=columna)
         if celda.value == encabezado_buscado:
             columna_encabezado = columna
             break
+    return columna_encabezado
 
 
 """crear los encabezados de la hoja de reporte"""
-def crear_encabezado(hoja : str, tabla : list):
-    #crear el titulo de la tabla donde se va a comparar
+def crear_encabezado(hoja : str, lista : list):
+    #crear el titulo de la lista donde se va a comparar
     columna = hoja.max_column
-    for encabezado in tabla:
+    for encabezado in lista:
         # print(encabezado)
         hoja.cell(row=1, column= columna, value=encabezado)
         columna+=1
 
 
+def rellenar_tabla (hoja : str, lista : list, diccionario):
+    crear_encabezado(hoja, lista)
+    
+    """crear variables que se van a usar"""
+    columna_total = buscar_columna(1, 'Total', hoja) #seleccionar la posicion de la columna
+    columna_clientes = buscar_columna(1, 'Clientes', hoja) #seleccionando la columna doonde se ingresan los nombres de los clientes
+    num = 2 #creando una variable para ir aumentando el
+    
+    """rellenar la columna de clientes en la hoja de Reporte"""
+    for clave in diccionario.keys(): #iterar sobre las keys del diccionario
+        persona = str(clave) #comvertir la key en un string
+        cliente = hoja.cell(row=num, column=columna_clientes) #seleccionar la casilla que se desea rellenar
+        cliente.value = persona
+        # print(num)
+        num = num + 1
+    
+    """rellenar la columna de los totales"""
+    for fila in range(2, hoja.max_row +1 ):
+        cliente = hoja.cell(row=fila, column=columna_clientes).value
+        total = hoja.cell(row=fila, column=columna_total)
 
+        total.value = diccionario[cliente]
+        
+        
 
 
--- repoete.py
+++ repoete.py
@@ -1,8 +1,10 @@
 from funciones.funciones import * #importar primero carpeta luego con el . se selecciona el archivo y el import es para extraer las funciones deseadas
 import openpyxl
+import datetime
 
 """variable creada para colocar el nomvre del archivo el cual se desea hacer el reporte"""
 nombre_archivo = "Libro2" 
+nombre_archivo_final = 'Reporte'
 
 libro = openpyxl.load_workbook(f'{nombre_archivo}.xlsx')
 hojas = libro.worksheets #crear la lista de las hojas de mi archivo
@@ -13,7 +15,7 @@ hoja_reporte = libro.create_sheet('Reporte')
 clientes ={}
 
 """lista para crear la rapla del reporte"""
-tabla_reporte = [ 
+lista_reporte = [ 
     'Clientes', 
     'Total',
     'Total declarado',
@@ -23,11 +25,19 @@ tabla_reporte = [
 """iterar sobre la tabla para llenar el diccionario de clientes con sus totales a pagar"""
 for fila in range(2, hoja.max_row +1): #iterar sobre la base de datos para llenar los datos del diccionario de clientes
     cliente = hoja.cell(row=fila, column=1).value #seleccionar los valores de la columna de clientes 
-    varlor = hoja.cell(row=fila, column=3).value #seleccionar los valores  de mi lista de valores
+    valor = hoja.cell(row=fila, column=3).value #seleccionar los valores  de mi lista de valores
     clientes.setdefault(cliente, 0) #ingresar las keys de mi diccionario de clientes
-    clientes[cliente] += varlor #sumar los valores encontrados de cada cliente e ingresarlo en el diccionario
+    
 
-crear_encabezado(hoja_reporte, tabla_reporte)
+    clientes[cliente] += valor #sumar los valores encontrados de cada cliente e ingresarlo en el diccionario
 
-prueba = hoja_reporte.cell(row=1, column=4).value
-print(prueba)
+"""llamar la funcion para rellenar la tabla"""
+rellenar_tabla(hoja_reporte, lista_reporte, clientes)
+
+# prueba = hoja_reporte.cell(row=2, column=2).value
+# print(prueba)
+
+"""fecha del reporte"""
+fecha_actual = datetime.date.today()
+
+libro.save(f'{nombre_archivo_final} {fecha_actual}.xlsx')
